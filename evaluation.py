dbdd9e0bd4522216f1cfdf252e02f565dd25256c
dshubenok <dshubenok93@gmail.com>
2016-03-04 17:24:41 +0300

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -13,30 +13,30 @@ file2 = os.path.join(basedir, file2)
 f = lambda s: s.strip().replace('=', ',').split(',')
 
 with open(file1) as f1, open(file2) as f2:
-    l1 = f1.readline()
-    l2 = f2.readline()
-    s = l = p = c = 0.0
-    while l1 and l2:
-        tla1 = l1.split(' ')
-        tla2 = l2.split(' ')
-        if len(tla1) == 3 and len(tla2) == 3:
-            t1, l1, a1 = tla1
-            t2, l2, a2 = tla2
-            if t1 == t2:
-                a1, a2 = f(a1), f(a2)
-                if l1 == l2:
-                    l += 1
+    line1 = f1.readline()
+    line2 = f2.readline()
+    s = lemma = pos = c = 0.0
+    while line1 and line2:
+        str1 = line1.split(' ')
+        str2 = line2.split(' ')
+        if len(str1) == 3 and len(str2) == 3:
+            word1, line1, tags1 = str1
+            word2, line2, tags2 = str2
+            if word1 == word2:
+                tags1, tags2 = f(tags1), f(tags2)
+                if line1 == line2:
+                    lemma += 1
                 w = 0
-                if a1 and a2 and a1[0].upper() == a1[0] and a1[0] == a2[0]:
-                    p += 1
-                for a in a1:
-                    if a in a2:
+                if tags1 and tags2 and tags1[0].upper() == tags1[0] and tags1[0] == tags2[0]:
+                    pos += 1
+                for a in tags1:
+                    if a in tags2:
                         w += weight
                 c += 1
                 s += w
-        l1 = f1.readline()
-        l2 = f2.readline()
-print "lemma - %f%% \n\tcount - %d" % (l / c, l)
-print "PoS-tagging - %f%% \n\tcount - %d" % (p / c, p)
+        line1 = f1.readline()
+        line2 = f2.readline()
+print "lemma - %f%% \n\tcount - %d" % (lemma / c, lemma)
+print "PoS-tagging - %f%% \n\tcount - %d" % (pos / c, p)
 print "annotation with weight - ", s / c
 print '-'*25, '\ncount - ', c
