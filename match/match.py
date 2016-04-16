c921914f26cd317a8e4325f5ba0f514093622188
akutuzov <akutuzov72@gmail.com>
2016-04-16 18:45:53 +0200

    Correct discovery of mis-aligned lines.

--- match/match.py
+++ match/match.py
@@ -1,16 +1,20 @@
-file1 = '1.txt'
-file2 = '2.txt'
+#!/usr/bin/python2
+#coding: utf-8
+
+import sys
+
+file1 = sys.argv[1] # Указываем имена файлов для сравнения
+file2 = sys.argv[2]
 
 with open(file1) as f1, open(file2) as f2:
-    line1 = f1.readline()
-    line2 = f2.readline() #читаем два файла построчно
-    c = 0.0 # устанавливаем начальное значения счетчика
-    while line1 and line2:
-        str1 = line1.split(' ')
-        str2 = line2.split(' ') # делим строки по пробелам на три части
-        if str1[0] == str2[0]: 
-            c += 1 # пока словоформы совпадают, прибавлять 1 к счетчику
+    text1 = f1.readlines()
+    text2 = f2.readlines() #читаем два файла построчно
+    c = 0  # устанавливаем начальное значения счетчика
+    for line1, line2 in zip(text1, text2):
+        str1 = line1.split()
+        str2 = line2.split() # делим строки по пробелам
+        c += 1 # пока словоформы совпадают, прибавлять 1 к счетчику
+        if str1[0] != str2[0]:
             print(c)
-
-        line1 = f1.readline()
-        line2 = f2.readline()
+            print str1[0].strip(), str2[0].strip()
+            break
